69af82afb9582cfc2aaedbfd3c615f3a679327a4
Hien Pham <hien.pham@favro.com>
2018-09-06 14:12:02 +0700

    init project

---
+++ src/components/Header/Header.js
@@ -0,0 +1,42 @@
+// IMPORT PACKAGE REFERENCES
+
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+// COMPONENT
+
+export const Header = () => (
+    <nav className="navbar navbar-expand-lg navbar-light">
+        <a className="navbar-brand" href="/">
+            <i className="fab fa-react fa-2x" style={{color: 'dodgerblue'}}></i>
+        </a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#menu">
+            <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="menu">
+            <ul className="navbar-nav ml-auto">
+                <li className="nav-item">
+                    <div className="nav-link">
+                        <NavLink to='/' activeClassName='menu selected' exact={true}>TRANG CHỦ</NavLink>
+                    </div>
+                </li>
+                <li className="nav-item">
+                    <div className="nav-link">
+                        <NavLink to='/zipcodes' activeClassName='menu selected'>TIN CÔNG NGHỆ</NavLink>
+                    </div>
+                </li>
+                <li className="nav-item">
+                    <div className="nav-link">
+                        <NavLink to='/zipcodes' activeClassName='menu selected'>GÓC LẬP TRÌNH</NavLink>
+                    </div>
+                </li>
+                <li className="nav-item">
+                    <div className="nav-link">
+                        <NavLink to='/about' activeClassName='menu selected'>GIỚI THIỆU</NavLink>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </nav>
+);
